44d0b4d71c9a079553f69df11b37f6917b0ea492
Dat Phan Minh <darkstardragon9@gmail.com>
2022-11-28 03:44:15 +0700

    update CMS

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-from flask import Flask, render_template, request, session,redirect
+from flask import Flask, render_template, request, session,redirect,url_for
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from forms import SignUpForm
 from random import randint
 import hashlib
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7a9097f3b37240fe8dbc99bc'
 client = MongoClient("mongodb+srv://dbadmin:H9kGaW0KH3wV1zpi@cluster0.sfcugwr.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
 db=client["WebDB"]
 webmaster = db["Webmaster"]
+content = db["Content"]
 @app.route('/')
 @app.route('/home')
 def home_page():
-    content = db["Content"]
+
     all_data = content.find({})
     return render_template("homepage.html",info = all_data)
 
@@ -74,6 +76,22 @@ def member_page():
     return render_template("about.html")
 
 @app.route('/cms', methods = ['GET', 'POST'])
-def cms_page():
-    if request.method == 'POST':
-        return render_template("base.html")
+@app.route('/cms/<pid>', methods = ['GET', 'POST'])
+def cms_page(pid="1"):
+    message =""
+    project_id=str(pid)
+    project_document = content.find_one({"id": pid})
+    if request.method == "POST":
+        header = request.form["header"]
+        link = request.form["link"]
+        description = request.form["description"]
+        #image = request.files["image"]
+        #if image:
+        #    image.save(os.path.join(app.root_path, "static/uploads/", image.filename))
+        #else:
+        if (pid!="0"):
+            content.update_one({"id":pid},{"$set":{"Header": header, "Link": link, "Description": description,"Img": "1"}})
+            message = f"Update Project {header} Succesfully"
+            return render_template("temp.html",info = content.find_one({"id": pid}),message=message)
+    return render_template("temp.html",info = project_document,message=message)
+
