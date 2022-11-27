d8fbbcaacf47306fff8aeb50815e001292c1249b
Dat Phan Minh <darkstardragon9@gmail.com>
2022-11-27 17:02:35 +0700

    rename

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://dbadmin:H9kGaW0KH3wV1zpi@cluster0.sfcugwr.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+db=client["WebDB"]
+user_pwd = db["Webmaster"]
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template("homepage.html")
+
+@app.route('/login')
+def login_page():
+    return render_template("login.html")
